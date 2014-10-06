18209547ded525064681eeefd090973ee9adb677
cristian <dos_cristian@live.com>
2014-10-06 23:52:46 +0200

    tercera revision

--- triviador/urls.py
+++ triviador/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.conf import settings
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'triviador.views.home', name='home'),
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('triviador.apps.trivia.urls')),
     url(r'^', include('triviador.apps.usuario.urls')),
-    #url(r'^$', indice),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root':settings.MEDIA_ROOT,}),	
 )
